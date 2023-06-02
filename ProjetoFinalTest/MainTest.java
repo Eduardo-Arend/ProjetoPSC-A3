2a4d790b5306adc881bdf2beaa9ba4ebf5bd927e
Eduardo Arend <educaa.az@hotmail.com>
2023-06-02 08:28:35 -0300

    Commit
    metodos de ordenaçao

--- ProjetoFinalTest/MainTest.java
+++ ProjetoFinalTest/MainTest.java
@@ -9,26 +9,45 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-public class MainTest {
+public class MainTest extends insertionSort {
     public static void main(String[] args) {
-        // TODO code application logic here
 
         File file = new File("C:\\projeto Final\\Teste.txt");
         Scanner sc = null;
-
         Date horaInicio = new Date();
         DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         String dataFormatadaInicial = dateFormat.format(horaInicio);
-
         System.out.println("Date em String formatada: " + dataFormatadaInicial);
         System.out.println("");
 
-        List<Long> numeros = new ArrayList<>();
         try {
             sc = new Scanner(file);
+            List<Long> modoInsertion = new ArrayList<>();
+            List<Long> modoQuick = new ArrayList<>();
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                numeros.add(Long.parseLong(line));
+                modoInsertion.add(Long.parseLong(line));
+
+                try {
+                    Long number = Long.parseLong(line);
+                    modoQuick.add(number);
+                } catch (NumberFormatException e) {
+                }
+            }
+            Long[] numerosArray = modoQuick.toArray(new Long[0]);
+            quickSort sorter = new quickSort();
+            sorter.QuickSort(numerosArray);
+
+            System.out.println("Array sorted using QuickSort:");
+            for (Long number : numerosArray) {
+                System.out.println(number);
+            }
+            Long[] arr = new Long[modoInsertion.size()];
+            InsertionSort(modoInsertion.toArray(arr));
+
+            System.out.println("Array sorted using Insertion Sort:");
+            for (Long num : arr) {
+                System.out.print(num + "\r\n");
             }
 
         } catch (IOException e) {
@@ -46,26 +65,5 @@ public class MainTest {
         System.out.println("");
         System.out.println("Date em String formatada: " + dataFormatadaFinal);
 
-        Long[] arr = new Long[numeros.size()];
-        insertionSort(numeros.toArray(arr));
-
-        System.out.println("Array sorted using Insertion Sort:");
-        for (Long num : arr) {
-            System.out.print(num + "\r\n");
-        }
-    }
-
-    public static void insertionSort(Long[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            Long key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
     }
 }
--- ProjetoFinalTest/insertionSort.java
+++ ProjetoFinalTest/insertionSort.java
@@ -1,30 +1,14 @@
 package ProjetoFinalTest;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
 
-public class insertionSort extends MainTest{
-    int [] arr = new int[10];
-    /**
-     * 
-     */
-    insertionSort(arr);
+public class insertionSort  {
 
-    System.out.println("Array sorted using Insertion Sort:");
-    for (int num : arr) {
-        System.out.print(num + " ");
-    }
-
-    public static void insertionSort(int[] arr) {
+    public static void InsertionSort(Long[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
-            int key = arr[i];
+            Long key = arr[i];
             int j = i - 1;
-    
+
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -33,5 +17,3 @@ public class insertionSort extends MainTest{
         }
     }
 }
-
-
